4c4d13109773348693c058ff7a3b7cbe6cb90ea5
Anwesh43 <anweshthecool0@gmail.com>
2017-04-14 18:55:40 +0530

    Adding State

---
+++ redux/src/main/java/com/anwesome/ui/redux/State.java
@@ -0,0 +1,11 @@
+package com.anwesome.ui.redux;
+
+/**
+ * Created by anweshmishra on 14/04/17.
+ */
+public class State<T> {
+    private T t;
+    public State(T t) {
+        this.t = t;
+    }
+}
