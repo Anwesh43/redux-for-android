c6c05c419e9980c983e8338b1ddff94cd4c784f0
Anwesh43 <anweshthecool0@gmail.com>
2017-04-14 19:10:47 +0530

    Adding Action class

---
+++ redux/src/main/java/com/anwesome/ui/redux/Action.java
@@ -0,0 +1,14 @@
+package com.anwesome.ui.redux;
+
+/**
+ * Created by anweshmishra on 14/04/17.
+ */
+public class Action {
+    private String type;
+    public String getType() {
+        return type;
+    }
+    public Action(String type) {
+        this.type = type;
+    }
+}
