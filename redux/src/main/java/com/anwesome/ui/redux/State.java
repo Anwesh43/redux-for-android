b15eb6fb664c595c527c6ab9cdf8684e8080cf47
Anwesh43 <anweshthecool0@gmail.com>
2017-04-14 20:32:55 +0530

    Modifying state class structure

--- redux/src/main/java/com/anwesome/ui/redux/State.java
+++ redux/src/main/java/com/anwesome/ui/redux/State.java
@@ -3,9 +3,13 @@ package com.anwesome.ui.redux;
 /**
  * Created by anweshmishra on 14/04/17.
  */
-public final class State<T> {
-    private T t;
-    public State(T t) {
-        this.t = t;
+public final class State {
+    private Object object;
+    public State(Object object) {
+        this.object = object;
     }
+    public Object get() {
+        return object;
+    }
+
 }
